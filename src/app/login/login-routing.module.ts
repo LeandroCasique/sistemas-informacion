fba0099ec944eaff662720c3a42ce99b4eed053d
oriana0anairo <ramiled0@gmail.com>
2021-03-19 18:56:05 -0400

    Estructura del sing in

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path:'sing-up',
+    component: SingUpComponent 
   }
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import {MatCardModule} from '@angular/material/card';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SingUpComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
