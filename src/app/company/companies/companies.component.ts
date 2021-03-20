a39af13c0ae76e84fc4cc179e83e3882fbb5bab7
leandrojcz <lenadro.casique@globalesm.com>
2021-03-20 14:38:35 -0400

    add servcie company

--- src/app/company/companies/companies.component.ts
+++ src/app/company/companies/companies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { CompaniesService } from 'src/app/services/companies.service';
 
 
 @Component({
@@ -7,10 +8,30 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./companies.component.css']
 })
 
-export class CompaniesComponent implements OnInit{
+export class CompaniesComponent implements OnInit {
+  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  public dataSource: any = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
 
+  constructor(private companiesService: CompaniesService) {
+  }
 
   ngOnInit() {
+    this.companiesService.getAllCompanies().subscribe(data => {
+      console.log(data);
+
+      //this.dataSource = data;
+    });
   }
 
 }
---
+++ src/app/services/companies.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompaniesService {
+
+  public apiUrl: string = environment.apiUrl;
+  public token: string;
+
+  constructor(private http: HttpClient, private auth: AuthService) {
+    this.token = this.auth.token;
+    console.log(this.token)
+  }
+
+  public getAllCompanies() {
+    return this.http.get(`${this.apiUrl}api/companies`, { headers: { token: this.token } });
+  }
+
+
+}
