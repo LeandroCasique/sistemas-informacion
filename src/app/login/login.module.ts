db87ccc87fc5f2da73bd10ed648609b6bf87731b
leandrojcz <lenadro.casique@globalesm.com>
2021-03-20 09:54:11 -0400

    change login and sing up

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,13 +5,16 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import {MatCardModule} from '@angular/material/card';
 import { SingUpComponent } from './sing-up/sing-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, SingUpComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class LoginModule { }
--- src/app/login/sing-up/sing-up.component.ts
+++ src/app/login/sing-up/sing-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingUpComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      Nb_Usuario: ['', Validators.required],
+      Tx_Email: ['', Validators.required],
+      Nu_Movil: [''],
+      Tx_Clave: ['', Validators.required],
+      Tx_ClaveConfirnm: ['', Validators.required],
+    }, {validator: this.passwordConfirming});
+  }
 
   ngOnInit(): void {
+  }
+
+  public signUp(){
+    if(this.form.valid) {
+      console.log(this.form);
+
+      let payload = {
+        Nb_Usuario: this.form.get('Nb_Usuario').value,
+        Tx_Email: this.form.get('Tx_Email').value,
+        Nu_Movil: this.form.get('Nu_Movil').value,
+        Tx_Clave: this.form.get('Tx_Clave').value,
+      }
+
+     /* this.user.signUp(payload).subscribe((data => {
+        console.log(data);
+      }));*/
+    }
+  }
+
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('Tx_Clave').value !== c.get('Tx_ClaveConfirnm').value) {
+        return {invalid: true};
+    }
   }
 
 }
