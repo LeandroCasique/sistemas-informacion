2e93d13d3b6ff5df389d9e601bcae5fef21c61c9
Mariana Castro <marianacezeta@gmail.com>
2021-03-21 07:32:59 -0400

    agregando tabla organización

--- src/app/company/organizations/organizations.component.ts
+++ src/app/company/organizations/organizations.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organizations.component.css']
 })
 export class OrganizationsComponent implements OnInit {
+  public displayedColumns: string[] = ['position', 'name', 'org_padre', 'empresa', 'tel', 'email', 'buttons'];
+  public dataSource: any = [
+    {position: 1, name: 'Te quiero Leandro :c', org_padre: 1, empresa: 'test', tel: 1234567, email: "test@test.com",  buttons: 'test'}
+  ];
 
   constructor() { }
 
